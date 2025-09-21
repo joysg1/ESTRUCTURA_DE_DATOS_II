62cd0f74601c7df3cc643eb369dad5b0206d8e5b
joysg1 <joy.nelaton@utp.ac.pa>
2025-09-21 16:59:17 -0500

    Add files via upload

---
+++ ARBOL INSERTAR/INSERTAR/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(39, 32)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(354, 252)
+        Me.TreeView1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(423, 32)
+        Me.TextBox1.MaxLength = 3
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(44, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(423, 74)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Insertar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Gold
+        Me.TextBox2.Location = New System.Drawing.Point(414, 152)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(36, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(456, 155)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Raiz principal"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.Blue
+        Me.TextBox3.Location = New System.Drawing.Point(414, 188)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(36, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(456, 191)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Nodo mayor"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.Color.Cyan
+        Me.TextBox4.Location = New System.Drawing.Point(414, 228)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(36, 20)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(456, 231)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Nodo menor"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(20, 40)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(101, 21)
+        Me.ComboBox1.TabIndex = 9
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(148, 38)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Eliminar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Location = New System.Drawing.Point(564, 32)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(239, 162)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Eliminacion"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(815, 325)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Name = "Form1"
+        Me.Text = "ÁRBOL"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ ARBOL INSERTAR/INSERTAR/Form1.vb
@@ -0,0 +1,204 @@
+﻿Public Class Form1
+    Dim nodP As New TreeNode
+    Dim nodPhi As New TreeNode
+    Dim nodPhd As New TreeNode
+    Dim nodPhi2 As New TreeNode
+    Dim nodPhid2 As New TreeNode
+    Dim nodPhdi As New TreeNode
+    Dim nodPhdd2 As New TreeNode
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TreeView1.Font = New Font("Arial", 20)
+
+
+
+        If TextBox1.Text = "" Then
+            MsgBox("Por favor ingrese un valor ha ingresar como nodo")
+
+        Else
+            If TreeView1.GetNodeCount(True) = 0 Then
+                nodP.Text = TextBox1.Text
+                Me.TreeView1.Nodes.Add(nodP)
+                nodP.BackColor = Color.Gold
+
+            ElseIf TreeView1.GetNodeCount(True) = 1 Then
+                nodP.Nodes.Add(nodPhi)
+                nodPhi.Text = TextBox1.Text
+                If Val(nodPhi.Text) > Val(nodP.Text) Then
+                    nodPhi.BackColor = Color.Blue
+                Else
+                    nodPhi.BackColor = Color.Cyan
+                End If
+
+            ElseIf TreeView1.GetNodeCount(True) = 2 Then
+                nodP.Nodes.Add(nodPhd)
+                nodPhd.Text = TextBox1.Text
+                If Val(nodPhd.Text) > Val(nodP.Text) Then
+                    nodPhd.BackColor = Color.Blue
+                Else
+                    nodPhd.BackColor = Color.Cyan
+                End If
+
+
+            ElseIf TreeView1.GetNodeCount(True) = 3 Then
+                nodPhi.Nodes.Add(nodPhi2)
+                nodPhi2.Text = TextBox1.Text
+                If Val(nodPhi2.Text) > Val(nodPhi.Text) Then
+                    nodPhi2.BackColor = Color.Blue
+                Else
+                    nodPhi2.BackColor = Color.Cyan
+                End If
+
+
+            ElseIf TreeView1.GetNodeCount(True) = 4 Then
+                nodPhi.Nodes.Add(nodPhid2)
+                nodPhid2.Text = TextBox1.Text
+                If Val(nodPhid2.Text) > Val(nodPhi.Text) Then
+                    nodPhid2.BackColor = Color.Blue
+                Else
+                    nodPhid2.BackColor = Color.Cyan
+                End If
+
+            ElseIf TreeView1.GetNodeCount(True) = 5 Then
+                nodPhd.Nodes.Add(nodPhdi)
+                nodPhdi.Text = TextBox1.Text
+                If Val(nodPhdi.Text) > Val(nodPhd.Text) Then
+                    nodPhdi.BackColor = Color.Blue
+                Else
+                    nodPhdi.BackColor = Color.Cyan
+                End If
+
+            ElseIf TreeView1.GetNodeCount(True) = 6 Then
+                nodPhd.Nodes.Add(nodPhdd2)
+                nodPhdd2.Text = TextBox1.Text
+
+                If Val(nodPhdd2.Text) > Val(nodPhd.Text) Then
+                    nodPhdd2.BackColor = Color.Blue
+                Else
+                    nodPhdd2.BackColor = Color.Cyan
+                End If
+
+            ElseIf TreeView1.GetNodeCount(True) = 7 Then
+
+
+
+                MsgBox("Ha alcanzado el nivel maximo de jerarquia, ahora puede eliminar nodos")
+                GroupBox1.Enabled = True
+                ComboBox1.Items.Add(nodP.Text)
+                ComboBox1.Items.Add(nodPhi.Text)
+                ComboBox1.Items.Add(nodPhd.Text)
+                ComboBox1.Items.Add(nodPhi2.Text)
+                ComboBox1.Items.Add(nodPhid2.Text)
+                ComboBox1.Items.Add(nodPhdi.Text)
+                ComboBox1.Items.Add(nodPhdd2.Text)
+
+
+
+
+
+
+            End If
+
+
+
+
+
+
+
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox2.Enabled = False
+        TextBox3.Enabled = False
+        TextBox4.Enabled = False
+        GroupBox1.Enabled = False
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If ComboBox1.SelectedIndex = 3 Then
+            TreeView1.Nodes.Remove(nodPhi2)
+
+        ElseIf ComboBox1.SelectedIndex = 4 Then
+            TreeView1.Nodes.Remove(nodPhid2)
+
+        ElseIf ComboBox1.SelectedIndex = 5 Then
+            TreeView1.Nodes.Remove(nodPhdi)
+
+        ElseIf ComboBox1.SelectedIndex = 6 Then
+            TreeView1.Nodes.Remove(nodPhdd2)
+
+        ElseIf ComboBox1.SelectedIndex = 1 Then
+            If Math.Abs(Val(nodPhi2.Text) - Val(nodPhi.Text)) < Math.Abs(Val(nodPhid2.Text) - Val(nodPhi.Text)) Then
+
+                nodPhi.Text = ""
+                nodPhi.Text = nodPhi2.Text
+                TreeView1.Nodes.Remove(nodPhi2)
+
+                If Val(nodPhi.Text) < Val(nodP.Text) Then
+                    nodPhi.BackColor = Color.Cyan
+
+                Else
+                    nodPhi.BackColor = Color.Blue
+                End If
+            Else
+                nodPhi.Text = ""
+                nodPhi.Text = nodPhid2.Text
+                TreeView1.Nodes.Remove(nodPhid2)
+                If Val(nodPhi.Text) < Val(nodP.Text) Then
+                    nodPhi.BackColor = Color.Cyan
+
+                Else
+                    nodPhi.BackColor = Color.Blue
+                End If
+
+            End If
+
+        ElseIf ComboBox1.SelectedIndex = 2 Then
+            If Math.Abs(Val(nodPhdi.Text) - Val(nodPhd.Text)) < Math.Abs(Val(nodPhdd2.Text) - Val(nodPhd.Text)) Then
+                nodPhd.Text = ""
+                nodPhd.Text = nodPhdi.Text
+                TreeView1.Nodes.Remove(nodPhdi)
+                If Val(nodPhd.Text) < nodP.Text Then
+                    nodPhd.BackColor = Color.Cyan
+                Else
+                    nodPhd.BackColor = Color.Blue
+                End If
+
+            Else
+                nodPhd.Text = ""
+                nodPhd.Text = nodPhdd2.Text
+                TreeView1.Nodes.Remove(nodPhdd2)
+                If Val(nodPhd.Text) < nodP.Text Then
+                    nodPhd.BackColor = Color.Cyan
+                Else
+                    nodPhd.BackColor = Color.Blue
+                End If
+
+            End If
+
+        ElseIf ComboBox1.SelectedIndex = 0 Then
+            MsgBox("No puede eliminar la raiz principal del arbol")
+
+        End If
+
+    End Sub
+End Class
