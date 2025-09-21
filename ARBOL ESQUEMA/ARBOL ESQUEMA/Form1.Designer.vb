17b940e42a59507d93c0bbe77785abad7f0266b8
joysg1 <joy.nelaton@utp.ac.pa>
2025-09-21 16:58:56 -0500

    Add files via upload

---
+++ ARBOL ESQUEMA/ARBOL ESQUEMA/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(47, 38)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(582, 293)
+        Me.TreeView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(287, 358)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(80, 35)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Crear "
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(671, 405)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Name = "Form1"
+        Me.Text = "ESTRUCTURAS DE DATOS II"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ ARBOL ESQUEMA/ARBOL ESQUEMA/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+
+    Private Sub TreeView1_AfterSelect(ByVal sender As System.Object, ByVal e As System.Windows.Forms.TreeViewEventArgs) Handles TreeView1.AfterSelect
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TreeView1.BackColor = Color.DeepSkyBlue
+        Dim nodo As New TreeNode("Abuelo")
+        Dim noda As New TreeNode("Padre 1")
+        Dim nodb As New TreeNode("Padre 2")
+        Dim noda1 As New TreeNode("Hijo 1a")
+        Dim noda2 As New TreeNode("Hijo 1b")
+        Dim nodb1 As New TreeNode("Hijo 2a")
+        Dim nodb2 As New TreeNode("Hijo 2b")
+
+        nodo.Nodes.Add(noda)
+        nodo.Nodes.Add(nodb)
+        noda.Nodes.Add(noda1)
+        noda.Nodes.Add(noda2)
+        nodb.Nodes.Add(nodb1)
+        nodb.Nodes.Add(nodb2)
+
+
+        TreeView1.Font = New Font("Arial", 20)
+        nodo.BackColor = Color.Gold
+        noda.BackColor = Color.Aquamarine
+        noda1.BackColor = Color.LightBlue
+        noda2.BackColor = Color.LightBlue
+        nodb.BackColor = Color.GreenYellow
+        nodb1.BackColor = Color.LightGreen
+        nodb2.BackColor = Color.LightGreen
+
+
+
+
+
+        Me.TreeView1.Nodes.Add(nodo)
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
